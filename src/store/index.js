6e6fdd7d6dbe972c7a8e967837a499bb3f827f44
applego <applegorillappa@gmail.com>
2019-12-03 11:24:26 +0900

    Update src/store/index.js:

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,26 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersistence from 'vuex-persist'
+import mutations from './mutations'
+import actions from './actions'
 
 Vue.use(Vuex)
+
+const debug = process.env.NODE_ENV !== 'production'
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage
+})
 
 export default new Vuex.Store({
   state: {
   },
   mutations: {
   },
-  actions: {
+  actions,
+  getters: {
   },
-  modules: {
-  }
+  plugins: [vuexLocal.plugin],
+  strict: debug
+  // modules: {}
 })
